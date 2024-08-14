7a900b569f88cdb93e5014daece1e953c22b18ec
Hanhtunaung97 <littlehanhtunaung97>
2024-08-14 16:33:55 +0800

    zustand add to categories section component

--- src/components/CategoriesButton.components.jsx
+++ src/components/CategoriesButton.components.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+import useCategoriesStore from "../store/useCategoriesStore";
 
 const CategoriesButtonComponents = ({
-  current,
+  category,
   children,
   className,
   ...rest
 }) => {
+  const { categoryActive } = useCategoriesStore();
+  const handleClick = () => {
+    categoryActive(category.id);
+  };
   return (
     <button
+      onClick={handleClick}
       {...rest}
       className={`${
-        current ? "bg-slate-800 " : "bg-slate-400"
+        category?.isActive ? "bg-slate-800 " : "bg-slate-400"
       } text-nowrap px-3 py-1 rounded-lg hover:bg-transparent hover:border hover:border-slate-400 duration-200 hover:text-slate-400 active:scale-90 active:bg-slate-400 active:text-white text-white ${className}`}
     >
       {children}
--- src/components/CategoriesSection.components.jsx
+++ src/components/CategoriesSection.components.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import CategoriesButtonComponents from "./CategoriesButton.components";
+import useCategoriesStore from "../store/useCategoriesStore";
 const title = "CategoriesSection";
-const categories = [
-  "electronics",
-  "jewelery",
-  "men's clothing",
-  "women's clothing",
-];
+
 const CategoriesSectionComponents = () => {
+  const {categories}=useCategoriesStore();
   return (
     <section className="flex justify-between flex-col gap-y-5 mb-5">
       <div className=" h-20" id="categories"></div>
       <h1 className="text-lg font-headings text-slate-500">{title}</h1>
       <div className=" flex gap-3 items-center overflow-x-scroll md:overflow-x-hidden category-buttons-group ">
-        <CategoriesButtonComponents current={true}>All</CategoriesButtonComponents>
+        {/* <CategoriesButtonComponents current={true}>All</CategoriesButtonComponents> */}
         {categories.map((category) => (
-          <CategoriesButtonComponents key={category} current={false}>
-            {category}
+          <CategoriesButtonComponents key={category.id} category={category}>
+            {category.name}
           </CategoriesButtonComponents>
         ))}
       </div>
